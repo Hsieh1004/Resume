51dec8ff997e739887a43ae3c080edc23f93613e
Hsieh <chian0221@gmail.com>
2022-03-31 14:26:55 +0800

    add

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "Hsieh", 
   description: "The description of the site.",
   head: [["link", { rel: "icon", href: `/logo.png` }]],
-  base: './Resume/',
+  base: '/Resume/',
   dest: "./dist",
 
 
