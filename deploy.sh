66378dc6e1d4b5de88d433c3601f8a36520f3ce2
Hsieh <chian0221@gmail.com>
2022-03-31 14:25:56 +0800

    add .

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,15 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+yarn docs:build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+cd dist
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://hsieh1004.github.io/Resume/
+git push -f https://github.com/Hsieh1004/Resume.git master:gh-pages
 cd -
